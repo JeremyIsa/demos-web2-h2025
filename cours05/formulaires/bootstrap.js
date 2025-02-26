da6d5d72bb71171e047496fb35a10f117bda1370
JeremyIsa <jeremy.isabelle257@outlook.com>
2025-02-26 08:01:48 -0500

    Cours du 24 février

--- cours05/formulaires/bootstrap.js
+++ cours05/formulaires/bootstrap.js
@@ -1,14 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const form = document.getElementById('myform');
+    const form = document.getElementById('myForm');
 
     form.addEventListener('submit', evt => {
         
+        evt.preventDefault();
+        evt.stopPropagation();
         if (!form.checkValidity()) {
-            evt.preventDefault();
-            evt.stopPropagation();
             
         }
+
+        form.classList.add('was-validated');            // Pour la validation Bootstrap
+                                                        // 1. Avoir Bootstrap installé
+                                                        // 2. Ajouter la classe 'needs-validation' au <form>
+                                                        // 3. Ajouter l'attribut 'novalidate' sur le <form>
+
+        // Validations personnalisées
+        // Vérifions que les 2 mots de passe sont identiques
+        const champmdp = document.getElementById('password');
+        const champmdp2 = document.getElementById('confirmPassword');
+        if (champmdp.value !== champmdp2.value) {
+            champmdp2.classList.remove('is-valid')
+            champmdp2.classList.add('is-invalid');
+            champmdp2.focus();
+            return;
+        }
+        else {
+            champmdp2.classList.add('is-valid');
+            champmdp2.classList.remove('is-invalid');
+        }
+
+        // Envoyer le formulaire valide
+        let data = new FormData(form);
+        for (const [cle, valeur]  of data.entries()) {
+            console.log(cle, "=", valeur);
+        }
+
+        form.reset();
+        form.classList.remove('was-validated');
+        return;
 
     });
 
